8049cde6a403a80d0dd2003a6518382f60d4cd31
Edwin Garcia <160422256+Edwin4156@users.noreply.github.com>
2024-03-20 21:15:02 -0600

    Add files via upload

---
+++ tarea 4.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+main(){/*{
+	int notas[4];  //indice o posicion del arreglo inicia en 0
+	notas [0] = 80;
+	notas [1] = 65;
+	notas [2] = 60;
+	notas [3] = 75;
+	ford(int i=0;i<4; i++){
+		cout<<i<<":"<< notas[i]<<endl;
+	}
+	cout<<notas[0]<<endl;
+	cout<<notas[1]<<endl;
+	cout<<notas[2]<<endl;
+	cout<<notas[3]<<endl;
+}
+main(){
+	int tam=0,suma=0,promedio=0;
+	cout<<"Cuanto notas desea ingresar:";
+	cin>>tam; //5
+	int notas[tam];
+	 for(int i=0;i<tam;i++){
+	 	cout<<"Ingrese Notas"<<i<<":";
+	 	cin>>notas[i];
+	 }
+	 for(int i=0;i<tam;i++){
+	    	suma +=notas[i];
+	 	
+	 }
+	 promedio = suma/ tam;
+	 cout<<"El promedio es "<<promedio<<endl;
+	 
+	int tam=0,suma=0,promedio=0;
+	int notas[tam]
+	char res;
+	do{
+		tam++;
+		cout<<"Ingresar notas"<<tam<<":";
+		cin>>notas[tam-];
+		cout<<"Deseas ingresar otras notas(s/n):"
+		cin>>res;
+	}	while(res== 's'||res 'S');
+	
+	 for(int i=0;i<tam;i++){
+	    	suma +=notas[i];
+		}
+	promedio = suma/ tam;
+	 cout<<"El promedio es "<<promedio<<endl;
+
+
+   string nombre;
+   int dato;
+   cout<<"ingrese dato";
+   cin>>dato;
+   cin.ignore();
+   cout<<"Ingrese Nombre completo:";
+   getline(cin,nombre);
+   
+   cout<<nombre<<endl;
+   
+   string semana[7]={"lunes","martes","miercoles","jueves","viernes","sabado","domindo"}
+   semana[3] = "jueves";
+    for (int i=0;<7;i++){
+   	     cout<<semana[i]<<end1;
+   }
+   */
+   int matriz[2][3]={{10,20,30,},{40,50,60}};
+   cout<<matriz[0][0]<<endl;
+   cout<<matriz[0][1]<<endl;
+   cout<<matriz[0][2]<<endl;
+   cout<<matriz[1][0]<<endl;
+   cout<<matriz[1][1]<<endl;
+   cout<<matriz[1][2]<<endl;
+   
+   
+   
+   
+   
+}
